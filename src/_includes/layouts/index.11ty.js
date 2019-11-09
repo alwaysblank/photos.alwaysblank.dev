dde2bac1ea00ef105800574109c993b923ffccd2
Ben Martinez-Bateman <ben@alwaysblank.org>
2019-11-09 15:18:26 -0800

    Remove forestry

--- src/_includes/layouts/index.11ty.js
+++ src/_includes/layouts/index.11ty.js
@@ -3,7 +3,7 @@ var cloudinary = require('cloudinary').v2;
 module.exports = cloudinary.api.resources({ 
   type: 'upload', 
   prefix: 'photos/fall-2019/' 
-}).then((res) => res.resources.map((image) => {
+}).then((res) => `${res.resources.map((image) => {
   return cloudinary.image(image.public_id, {
     transformation: [
       {
@@ -13,4 +13,4 @@ module.exports = cloudinary.api.resources({
       }
     ]
   });
-}));
+})}`);
