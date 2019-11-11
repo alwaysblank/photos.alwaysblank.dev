91083058228eca4c8ba84d32c080ed13a41bc41e
Ben Martinez-Bateman <ben@alwaysblank.org>
2019-11-10 19:13:49 -0800

    Locally cache API results for individual rolls

--- src/_includes/layouts/gallery.11ty.js
+++ src/_includes/layouts/gallery.11ty.js
@@ -5,24 +5,49 @@ class Test {
     };
   }
 
-  render({album, content}) {
+  template(content, images) {
+    return `<div>${content}</div><div>${images}</div>`
+  }
+
+  buildImage(image) {
     const cloudinary = require('cloudinary').v2;
 
-    return cloudinary.api.resources({ 
-      type: 'upload', 
-      prefix: album 
-    }).then((res) => `<div>${content}</div><div>${res.resources.map((image) => {
-      const src = cloudinary.url(image.public_id, {
-        transformation: [{ width: 100,}]
+    const src = cloudinary.url(image, {
+      transformation: [{ width: 100,}]
+    });
+    const srcset = [320, 640, 1280].map(w => {
+      const src = cloudinary.url(image, {
+        transformation: [{ width: w,}]
       });
-      const srcset = [320, 640, 1280].map(w => {
-        const src = cloudinary.url(image.public_id, {
-          transformation: [{ width: w,}]
-        });
-        return `${src} ${w}w`;
+      return `${src} ${w}w`;
+    });
+    return `<img class="border-4 border-gray-400 my-4" 
+              src="${src}" 
+              srcset="${srcset.join(`, `)}" 
+              sizes="(max-width: 640px) 100vw, 640px" />`;
+  }
+
+  render({album, content, page, photos}) {
+    const fs = require('fs');
+    const cloudinary = require('cloudinary').v2;
+    const data_file = `./src${page.filePathStem}.11tydata.json`;
+    
+    if (photos) {
+      console.log('loaded from cache');
+      return this.template(content, photos.map(this.buildImage).join(``))
+    } else {
+      console.log('getting from cloludinary');
+      return cloudinary.api.resources({ 
+        type: 'upload', 
+        prefix: album 
       })
-      return `<img class="border-4 border-gray-400 my-4" src="${src}" srcset="${srcset.join(`, `)}" sizes="(max-width: 640px) 100vw, 640px" />`;
-    }).join(``)}</div>`);
+      .then(res => {
+
+        fs.writeFile(data_file, JSON.stringify({"photos": res.resources.map(asset => asset.public_id)}), err => console.log(err));
+        return res;
+      })
+      .then((res) => this.template(content, res.resources.map(asset => asset.public_id).map(this.buildImage).join(``)));
+    }
   }
 }
 
